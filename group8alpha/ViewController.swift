9933957ebb61d7bf38348080f15c100da3e20dd7
Eder Garza <edergarza@macbook-pro.local>
2016-10-23 15:33:56 -0500

    Initial Commit

---
+++ group8alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  group8alpha
+//
+//  Created by Eder Garza on 10/23/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
