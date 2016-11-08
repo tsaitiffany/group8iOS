52909eb93f39818919cf90259996a3ddfe75acf2
Adam Thompson <apt443@viisi.cs.utexas.edu>
2016-11-07 21:04:38 -0600

    Image saving to core data implemented (probably)

---
+++ group8alpha/Thumbnail.swift
@@ -0,0 +1,17 @@
+//
+//  Thumbnail.swift
+//  group8alpha
+//
+//  Created by Adam Thompson on 11/7/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Thumbnail)
+class Thumbnail: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
