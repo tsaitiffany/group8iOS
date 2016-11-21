97ba5981ff269e90777220e8bf09d8a437d1d0a1
Tiffany <tsaitiffany@utexas.edu>
2016-11-21 11:33:31 -0600

    what was turned in for beta

---
+++ group8alpha/fawViewController.swift
@@ -0,0 +1,35 @@
+//
+//  fawViewController.swift
+//  group8alpha
+//
+//  Created by Eder Garza on 11/12/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+
+class fawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
